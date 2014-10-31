aa62ca1ae1c9aecf529b5719409bded82689d2f4
dvoraka <alen.dvorak@gmail.com>
2014-10-31 23:35:08 +0100

    Code layout

--- tests/test_pyrt.py
+++ tests/test_pyrt.py
@@ -18,14 +18,14 @@ class TestTicket(unittest.TestCase):
 
         with self.assertRaises(TypeError):
 
-            ticket = pyrt.Ticket(
+            pyrt.Ticket(
                 '+ěščřž',
                 '+ěščřž',
                 {'žř': 'žř'},
                 None,
             )
 
-        ticket = pyrt.Ticket(
+        pyrt.Ticket(
             '+ěščřž',
             '+ěščřž',
             {'žř': 'žř'},
@@ -33,7 +33,7 @@ class TestTicket(unittest.TestCase):
         )
 
     def test_map_data(self):
-        
+
         ticket = pyrt.Ticket(
             '+ěščřž',
             '+ěščřž',
@@ -46,7 +46,7 @@ class TestTicket(unittest.TestCase):
 
 
 class TestTicketHistory(unittest.TestCase):
-    
+
     def setUp(self):
 
         self.th = pyrt.TicketHistory(None, None)
@@ -57,18 +57,18 @@ class TestTicketList(unittest.TestCase):
     def setUp(self):
 
         data = {
-                '10': 'Test 1',
-                '25': 'test 2',
-                '222555': 'Very long subject',
-                'five': 'test',
-                '1': 'text',
-                '2': '+ěščřž',
+            '10': 'Test 1',
+            '25': 'test 2',
+            '222555': 'Very long subject',
+            'five': 'test',
+            '1': 'text',
+            '2': '+ěščřž',
         }
 
         self.tl = pyrt.TicketList(data, pyrt.RT4())
 
     def test_list_all(self):
-        
+
         out = self.tl.list_all()
         aout = (
             (10, 'Test 1'),
@@ -86,9 +86,9 @@ class TestRT4(unittest.TestCase):
     def setUp(self):
 
         self.rt = pyrt.RT4()
-   
+
     def test_login(self):
-        
+
         self.rt.login('test', 'testpass')
         aout = {'user': 'test', 'pass': 'testpass'}
         self.assertEqual(self.rt.credentials, aout)
@@ -122,7 +122,7 @@ class TestRT4(unittest.TestCase):
             reply = self.rt.check_reply(text)
 
     def test_history_id(self):
-        
+
         text = 'test'
         reply = self.rt._history_id(text)
         self.assertEqual(reply, None)
@@ -152,7 +152,7 @@ class TestRT4(unittest.TestCase):
         self.assertEqual(reply, None)
 
     def test_parse_reply(self):
-        
+
         text = ''
         reply = self.rt.parse_reply(text)
         self.assertEqual(reply, None)
@@ -200,27 +200,32 @@ class TestRT4(unittest.TestCase):
 #            reply = self.rt.parse_reply(text)
 
     def test_parse_history_reply(self):
-        
+
         text = ''
         out = self.rt.parse_history_reply(text)
         aout = None
         self.assertEqual(out, aout)
- 
-        text = ('RT/4.0 200 ok\n\nid: 10\n\nTicket: 1234567890\n'
+
+        text = (
+            'RT/4.0 200 ok\n\nid: 10\n\nTicket: 1234567890\n'
             'Type: Create\nCreator: tuser\n'
             'Description: Correspondence\n'
             'Created: 2013-06-20 06:35:11\n'
         )
         out = self.rt.parse_history_reply(text)
-        aout = {'10': {'id': '10',
-            'Ticket': '1234567890', 'Type': 'Create',
-            'Creator': 'tuser',
-            'Description': 'Correspondence',
-            'Created': '2013-06-20 06:35:11',
-        }}
+        aout = {
+            '10': {
+                'id': '10',
+                'Ticket': '1234567890', 'Type': 'Create',
+                'Creator': 'tuser',
+                'Description': 'Correspondence',
+                'Created': '2013-06-20 06:35:11',
+            }
+        }
         self.assertEqual(out, aout)
- 
-        text = ('RT/4.0 200 ok\n\nid: 10\n\nTicket: 1234567890\n'
+
+        text = (
+            'RT/4.0 200 ok\n\nid: 10\n\nTicket: 1234567890\n'
             'Type: Create\n\n'
             'Content: First line\n  Second line\n  \n  Third line.\n'
             'Next: aaa\n'
@@ -229,25 +234,27 @@ class TestRT4(unittest.TestCase):
             'Created: 2013-06-20 06:35:11\n'
         )
         out = self.rt.parse_history_reply(text)
-        aout = {'10': {'id': '10',
-            'Ticket': '1234567890', 'Type': 'Create',
-            'Content': 'First line\nSecond line\n\nThird line.\n',
-            'Creator': 'tuser',
-            'Description': 'Correspondence added',
-            'Created': '2013-06-20 06:35:11',
-
-        }}
+        aout = {
+            '10': {
+                'id': '10',
+                'Ticket': '1234567890', 'Type': 'Create',
+                'Content': 'First line\nSecond line\n\nThird line.\n',
+                'Creator': 'tuser',
+                'Description': 'Correspondence added',
+                'Created': '2013-06-20 06:35:11',
+            }
+        }
         self.assertEqual(out, aout)
 
         with self.assertRaises(pyrt.BadRequestException):
 
             text = 'RT/4.0 400 Bad request\n\nReason:\n'
-            reply = self.rt.parse_history_reply(text)
+            self.rt.parse_history_reply(text)
 
         self.assertEqual(out, aout)
 
     def test_strip_hashes(self):
-        
+
         text = ''
         out = self.rt._strip_hashes(text)
         aout = ''
